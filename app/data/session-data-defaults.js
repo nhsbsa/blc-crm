b7d2e749a3ad671c36f82193cfaf8f01640ccbf0
Matt Fielding <matt@hippodigital.co.uk>
2023-11-15 17:07:22 +0000

    added some test data

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,294 @@
 module.exports = {
-
-  // Insert values here
-
+  "applications": [
+    {
+      "referenceNumber": "J4R-8H5-2K9F1",
+      "firstName": "John",
+      "lastName": "Doe",
+      "address": "123 Main Street, Apartment 4B, Riverside Towers, Suite 1234",
+      "postcode": "AB12 3CD",
+      "dateOfBirth": "1985-05-10",
+      "emailAddress": "johndoe@example.com",
+      "status": "Active"
+    },
+    {
+      "referenceNumber": "S7T-1P2-6Q3R4",
+      "firstName": "Maria",
+      "lastName": "Gomez",
+      "address": "456 Elm Avenue, Unit 7, Lakeview Residences",
+      "postcode": "EF34 5GH",
+      "dateOfBirth": "1990-07-15",
+      "emailAddress": "mariagomez@example.com",
+      "status": "Pending"
+    },
+    {
+      "referenceNumber": "M3N-5O6-7P8Q9",
+      "firstName": "Muhammad",
+      "lastName": "Ali",
+      "address": "789 Oak Road, Suite 456, Forest Heights",
+      "postcode": "IJ67 8KL",
+      "dateOfBirth": "1978-12-20",
+      "emailAddress": "muhammadali@example.com",
+      "status": "Expired"
+    },
+    {
+      "referenceNumber": "R1S-9U2-3V4W5",
+      "firstName": "Sofia",
+      "lastName": "Rodriguez",
+      "address": "321 Pine Lane, Apt 12, Mountainview Apartments",
+      "postcode": "KL45 6MN",
+      "dateOfBirth": "1995-03-25",
+      "emailAddress": "sofiarodriguez@example.com",
+      "status": "Cancelled"
+    },
+    {
+      "referenceNumber": "K6L-2Y8-5Z3X7",
+      "firstName": "Chen",
+      "lastName": "Li",
+      "address": "567 Cedar Street, Suite 789, Lakefront Suites",
+      "postcode": "XY89 0OP",
+      "dateOfBirth": "1980-08-05",
+      "emailAddress": "chenli@example.com",
+      "status": "Active"
+    },
+    {
+      "referenceNumber": "Q3A-7B8-6C9D0",
+      "firstName": "Aisha",
+      "lastName": "Khan",
+      "address": "987 Birch Avenue, Apt 345, Hillside Residences",
+      "postcode": "OP23 4QR",
+      "dateOfBirth": "1973-11-15",
+      "emailAddress": "aishakhan@example.com",
+      "status": "Expired"
+    },
+    {
+      "referenceNumber": "T0M-5N6-8P1Q2",
+      "firstName": "Liam",
+      "lastName": "O'Connor",
+      "address": "741 Oak Lane, Unit 234, Parkside Apartments",
+      "postcode": "CD56 7EF",
+      "dateOfBirth": "1989-09-30",
+      "emailAddress": "liamoconnor@example.com",
+      "status": "Cancelled"
+    },
+    {
+      "referenceNumber": "E8X-4P5-2Z6V3",
+      "firstName": "Sophia",
+      "lastName": "Kim",
+      "address": "101 Elm Street, Apt 8, Willow Residences",
+      "postcode": "GH12 9IJ",
+      "dateOfBirth": "1988-06-12",
+      "emailAddress": "sophiakim@example.com",
+      "status": "Active"
+    },
+    {
+      "referenceNumber": "H9U-3B2-7G8T1",
+      "firstName": "Ahmed",
+      "lastName": "Mahmoud",
+      "address": "567 Pine Avenue, Suite 23, Riverfront Heights",
+      "postcode": "KL34 5MN",
+      "dateOfBirth": "1992-04-27",
+      "emailAddress": "ahmedmahmoud@example.com",
+      "status": "Pending"
+    },
+    {
+      "referenceNumber": "N6Z-8L7-1Y2K9",
+      "firstName": "Elena",
+      "lastName": "Ivanova",
+      "address": "789 Oak Lane, Apt 56, Greenview Apartments",
+      "postcode": "WX45 6CD",
+      "dateOfBirth": "1987-09-18",
+      "emailAddress": "elenaivanova@example.com",
+      "status": "Expired"
+    },
+    {
+      "referenceNumber": "D1S-6X2-9F4W7",
+      "firstName": "Raj",
+      "lastName": "Patel",
+      "address": "234 Cedar Street, Apt 7, Lakeside Residences",
+      "postcode": "YZ67 8EF",
+      "dateOfBirth": "1983-03-03",
+      "emailAddress": "rajpatel@example.com",
+      "status": "Cancelled"
+    },
+    {
+      "referenceNumber": "L5J-4C8-2P3Q9",
+      "firstName": "Isabella",
+      "lastName": "Fernandez",
+      "address": "456 Birch Road, Unit 89, Sunset Apartments",
+      "postcode": "OP12 4QR",
+      "dateOfBirth": "1994-11-29",
+      "emailAddress": "isabellafernandez@example.com",
+      "status": "Active"
+    },
+    {
+      "referenceNumber": "T7O-2N6-8D1Q5",
+      "firstName": "Yusuf",
+      "lastName": "Khan",
+      "address": "789 Pine Avenue, Apt 345, Hillside Residences",
+      "postcode": "MN34 5OP",
+      "dateOfBirth": "1991-02-17",
+      "emailAddress": "yusufkhan@example.com",
+      "status": "Pending"
+    },
+    {
+      "referenceNumber": "B3A-7K8-6T9D0",
+      "firstName": "Mia",
+      "lastName": "Brown",
+      "address": "876 Oak Street, Suite 12, Riverfront Towers",
+      "postcode": "AB56 7XY",
+      "dateOfBirth": "1986-07-08",
+      "emailAddress": "miabrown@example.com",
+      "status": "Expired"
+    },
+    {
+      "referenceNumber": "K0M-9L6-5P8Q2",
+      "firstName": "Ali",
+      "lastName": "Abdullah",
+      "address": "987 Elm Lane, Apt 234, Lakeside Residences",
+      "postcode": "EF78 6KL",
+      "dateOfBirth": "1982-05-22",
+      "emailAddress": "aliabdullah@example.com",
+      "status": "Cancelled"
+    },
+    {
+      "referenceNumber": "P2S-5B8-6W9V0",
+      "firstName": "Grace",
+      "lastName": "Davis",
+      "address": "456 Pine Avenue, Apt 56, Willow Residences",
+      "postcode": "WX89 0CD",
+      "dateOfBirth": "1993-10-12",
+      "emailAddress": "gracedavis@example.com",
+      "status": "Active"
+    },
+    {
+      "referenceNumber": "R6A-3B8-2C9D0",
+      "firstName": "Abdul",
+      "lastName": "Saeed",
+      "address": "234 Oak Road, Suite 78, Riverfront Heights",
+      "postcode": "MN12 4OP",
+      "dateOfBirth": "1997-03-08",
+      "emailAddress": "abdulsaeed@example.com",
+      "status": "Pending"
+    },
+    {
+      "referenceNumber": "L0M-8N6-4P1Q2",
+      "firstName": "Lily",
+      "lastName": "Mendoza",
+      "address": "789 Birch Lane, Apt 23, Lakeside Residences",
+      "postcode": "YZ45 6KL",
+      "dateOfBirth": "1984-08-30",
+      "emailAddress": "lilymendoza@example.com",
+      "status": "Expired"
+    },
+    {
+      "referenceNumber": "T3A-7Y8-6B9D0",
+      "firstName": "Hassan",
+      "lastName": "Ali",
+      "address": "987 Pine Street, Apt 345, Sunset Apartments",
+      "postcode": "WX67 8OP",
+      "dateOfBirth": "1996-12-15",
+      "emailAddress": "hassanali@example.com",
+      "status": "Cancelled"
+    },
+    {
+      "referenceNumber": "N0M-5Z6-8X1Q2",
+      "firstName": "Olivia",
+      "lastName": "Cruz",
+      "address": "123 Cedar Road, Suite 56, Willow Residences",
+      "postcode": "CD34 5GH",
+      "dateOfBirth": "1981-04-25",
+      "emailAddress": "oliviacruz@example.com",
+      "status": "Active"
+    },
+    {
+      "referenceNumber": "E9R-7C8-2M9D0",
+      "firstName": "Youssef",
+      "lastName": "Nasr",
+      "address": "456 Oak Lane, Apt 89, Riverfront Towers",
+      "postcode": "EF56 7KL",
+      "dateOfBirth": "1988-09-20",
+      "emailAddress": "youssefnasr@example.com",
+      "status": "Pending"
+    },
+    {
+      "referenceNumber": "G0T-5N6-8A1B2",
+      "firstName": "Sophie",
+      "lastName": "Garcia",
+      "address": "789 Elm Avenue, Suite 123, Forest Heights",
+      "postcode": "KL23 4QR",
+      "dateOfBirth": "1983-11-05",
+      "emailAddress": "sophiegarcia@example.com",
+      "status": "Expired"
+    },
+    {
+      "referenceNumber": "K1L-2D8-5Y3X7",
+      "firstName": "Yara",
+      "lastName": "Nasser",
+      "address": "321 Birch Street, Apt 45, Lakeside Residences",
+      "postcode": "WX67 8CD",
+      "dateOfBirth": "1990-06-07",
+      "emailAddress": "yaranasser@example.com",
+      "status": "Cancelled"
+    },
+    {
+      "referenceNumber": "P3R-5E6-9Z8V7",
+      "firstName": "Ethan",
+      "lastName": "Davis",
+      "address": "567 Cedar Road, Apt 56, Riverside Towers",
+      "postcode": "AB45 6XY",
+      "dateOfBirth": "1985-02-14",
+      "emailAddress": "ethandavis@example.com",
+      "status": "Active"
+    },
+    {
+      "referenceNumber": "R9A-1B8-6S9W0",
+      "firstName": "Lara",
+      "lastName": "Mustafa",
+      "address": "234 Oak Avenue, Suite 23, Parkside Apartments",
+      "postcode": "GH34 5MN",
+      "dateOfBirth": "1993-07-26",
+      "emailAddress": "laramustafa@example.com",
+      "status": "Pending"
+    },
+    {
+      "referenceNumber": "L2M-8N6-5P1Q2",
+      "firstName": "Aidan",
+      "lastName": "Smith",
+      "address": "876 Elm Lane, Apt 45, Hillside Residences",
+      "postcode": "IJ45 6KL",
+      "dateOfBirth": "1982-12-03",
+      "emailAddress": "aidansmith@example.com",
+      "status": "Expired"
+    },
+    {
+      "referenceNumber": "T7A-7B8-6C9D0",
+      "firstName": "Sophie",
+      "lastName": "Brown",
+      "address": "456 Pine Road, Suite 78, Greenview Apartments",
+      "postcode": "WX67 8EF",
+      "dateOfBirth": "1996-05-19",
+      "emailAddress": "sophiebrown@example.com",
+      "status": "Cancelled"
+    },
+    {
+      "referenceNumber": "B0M-5L6-8P1Q2",
+      "firstName": "Oscar",
+      "lastName": "Gomez",
+      "address": "789 Cedar Street, Apt 234, Willow Residences",
+      "postcode": "YZ23 4QR",
+      "dateOfBirth": "1989-08-28",
+      "emailAddress": "oscargomez@example.com",
+      "status": "Active"
+    },
+    {
+      "referenceNumber": "K3A-7M8-6N9D0",
+      "firstName": "Maya",
+      "lastName": "Patel",
+      "address": "987 Elm Road, Suite 56, Forest Heights",
+      "postcode": "GH56 7OP",
+      "dateOfBirth": "1984-03-16",
+      "emailAddress": "mayapatel@example.com",
+      "status": "Pending"
+    }
+  ]
 }
