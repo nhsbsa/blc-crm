5eabd84a541859a2c9ab4e194568155d3d6e0b1d
Matt Fielding <matt@hippodigital.co.uk>
2023-11-15 17:07:48 +0000

    output data into the console

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,16 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+router.use((req, res, next) => {
+  const log = {
+    method: req.method,
+    url: req.originalUrl,
+    data: req.session.data
+  }
+  // you can enable this in your .env file
+  console.log(JSON.stringify(log, null, 2))
+  next()
+})
+
+
 // Add your routes here
