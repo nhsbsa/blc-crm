65b851a4c92bd730e9fa5bef0b4ade8d97cd8575
Matt Fielding <matt@hippodigital.co.uk>
2023-11-15 17:07:34 +0000

    created a filter for the tags

--- app/filters.js
+++ app/filters.js
@@ -8,3 +8,15 @@ const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
 
+addFilter('uppercase', function (content) {
+  return content.toUpperCase()
+})
+
+addFilter('tag', function (content) {
+  let colour = 'blue'
+  if (content === 'Active') {
+    colour = 'green'
+  }
+  let tagCode = '<strong class="govuk-tag govuk-tag--'+ colour +'">' + content + '</strong>'
+  return tagCode
+})
