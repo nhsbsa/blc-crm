8e2482079cc918e8ce60b8ecc84be442b2be639e
Matt Fielding <matt@hippodigital.co.uk>
2023-11-16 07:31:53 +0000

    50 results if you submit a blank form

--- app/routes.js
+++ app/routes.js
@@ -20,6 +20,7 @@ router.post("/v2/get-results", function (req, res) {
   let term = ''
   // load all applications
   let applications = req.session.data['applications']
+  console.log('applications: ' + applications.length)
   // create an empty array to store matching results
   let results = []
 
@@ -69,10 +70,37 @@ router.post("/v2/get-results", function (req, res) {
   if (term !== '') {
     req.session.data['results'] = results
   } else {
-    req.session.data['results'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+    req.session.data['results'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49]
   }
   // then return the user to the results page
   res.redirect('/v2/results2')
 });
+
+function compareStrings(a, b) {
+  // allow case-insensitive comparison
+  a = a.toLowerCase();
+  b = b.toLowerCase();
+  return (a < b) ? -1 : (a > b) ? 1 : 0;
+}
+
+// build a function to sort the results into a different order
+router.get('/v2/results2', function (req, res) {
+  // check for data
+  let resultsSorted = req.session.data['results']
+  let results = ''
+  //
+  if (req.query.sorted) {
+    var sorting = req.query.sorted
+  }
+
+  // resultsSorted.sort(function(a, b) {
+  //   return compareStrings(a.lastName, b.lastName);
+  // })
+
+  return res.render('/v2/results2', {
+    'results': resultsSorted
+  })
+})
+
 
 
